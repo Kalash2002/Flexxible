451b29bd8dfa5ad39aa0c0febcc77e8077de7060
Kalash2002 <202051097@iiitvadodara.ac.in>
2023-09-27 23:53:51 +0530

    changes in array

--- graphql/index.ts
+++ graphql/index.ts
@@ -55,8 +55,8 @@ export const createUserMutation = `
 `;
 
 export const projectsQuery = `
-  query getProjects($categories: String, $endcursor: String) {
-    projectSearch(first: 8, after: $endcursor, filter: {category: {eq: $categories}}) {
+  query getProjects($categories: [String], $endcursor: String) {
+    projectSearch(first: 10, after: $endcursor, filter: {category: {in: $categories}}) {
       pageInfo {
         hasNextPage
         hasPreviousPage
